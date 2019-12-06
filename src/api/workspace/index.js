d57e57d3d390f9adcc3d88535afcd4e9cbb90c37
Dennis <dennis.tgt@gmail.com>
2019-12-06 23:00:52 +0700

    remove .env

--- src/api/workspace/index.js
+++ src/api/workspace/index.js
@@ -11,7 +11,7 @@ class WorkspaceAPI{
   static async create(name){
     const response = await fetch(process.env.REACT_APP_API_URL + "workspaces", {
         method: 'POST',
-        body: JSON.stringify({name: name}),
+        body: JSON.stringify({ name: name }),
         headers: { 
           'Content-Type': 'application/json',
           'Authorization': 'Bearer ' + AuthAPI.getUserToken()
@@ -24,6 +24,41 @@ class WorkspaceAPI{
   static async get(){
     const response = await fetch(process.env.REACT_APP_API_URL + "workspaces", {
         method: 'GET',
+        headers: { 
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + AuthAPI.getUserToken()
+        }
+    })
+    
+    return ResponseAPI.handleResponse(response)
+  }
+
+  /**
+   * 
+   * @param {string} id 
+   * @param {string} name 
+   */
+  static async update(id, name){
+    const response = await fetch(process.env.REACT_APP_API_URL + "workspaces/update", {
+        method: 'POST',
+        body: JSON.stringify({ id, name }),
+        headers: { 
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + AuthAPI.getUserToken()
+        }
+    })
+    
+    return ResponseAPI.handleResponse(response)
+  }
+
+  /**
+   * 
+   * @param {string} id 
+   */
+  static async destroy(id){
+    const response = await fetch(process.env.REACT_APP_API_URL + "workspaces/destroy", {
+        method: 'POST',
+        body: JSON.stringify({ id }),
         headers: { 
           'Content-Type': 'application/json',
           'Authorization': 'Bearer ' + AuthAPI.getUserToken()
